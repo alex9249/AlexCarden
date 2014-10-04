9133f8891f3fb13f5e8068c99814b63b1a353581
alex9249 <alex9249@hotmail.com>
2014-10-04 13:51:35 -0400

    First Commit

---
+++ AlexCarden/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AlexCarden.MvcApplication" Language="C#" %>
---
+++ AlexCarden/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AlexCarden.Startup))]
+namespace AlexCarden
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
